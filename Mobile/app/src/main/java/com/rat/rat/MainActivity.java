601e8aa9f63456a7185b0551b99e538ef0c93816
Mathieu Sanchez <mathieu.sanchez@epitech.eu>
2018-02-25 00:18:00 +0100

    Suppression import inutile

--- Mobile/app/src/main/java/com/rat/rat/MainActivity.java
+++ Mobile/app/src/main/java/com/rat/rat/MainActivity.java
@@ -15,10 +15,6 @@ import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.firestore.DocumentSnapshot;
 import com.google.firebase.firestore.FirebaseFirestore;
-import com.google.firebase.firestore.QuerySnapshot;
-
-import java.util.HashMap;
-import java.util.Map;
 
 public class MainActivity extends AppCompatActivity {
 
